b66bd7ad0680cd9ff643ac5bb634291a22a4d8ff
Alser TagAlser <Alser@TagAlser>
2022-09-22 18:19:58 +0300

    ..

--- js/style.js
+++ js/style.js
@@ -1,5 +1,9 @@
 const toggleBar = document.querySelector(".toggle-bars");
 const navbar = document.querySelector("nav");
+
 toggleBar.addEventListener("click", () => {
   navbar.classList.toggle("hidden");
 });
+navbar.onclick = function () {
+  return navbar.classList.remove("hidden");
+};
