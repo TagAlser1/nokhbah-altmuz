2f5404fa467cebf627eed75d278e672cf8ecc2da
Alser TagAlser <Alser@TagAlser>
2022-09-23 14:04:10 +0300

    add background auto change

--- js/style.js
+++ js/style.js
@@ -1,5 +1,6 @@
 const toggleBar = document.querySelector(".toggle-bars");
 const navbar = document.querySelector("nav");
+let heroImage = document.getElementById("home");
 
 toggleBar.addEventListener("click", () => {
   navbar.classList.toggle("hidden");
@@ -7,3 +8,7 @@ toggleBar.addEventListener("click", () => {
 navbar.onclick = function () {
   return navbar.classList.remove("hidden");
 };
+setInterval(() => {
+  let backNum = Math.trunc(Math.random() * 4) + 1;
+  heroImage.style.backgroundImage = `url("../imgs/background-${backNum}.jpg")`;
+}, 2000);
